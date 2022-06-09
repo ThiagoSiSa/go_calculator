dd56d136dc34e82ae39637439a47e5fc5918be0b
Thiago <santos.auto@hotmail.com>
2022-06-09 14:44:25 -0300

    feature - Add Tokenizer Co-authored-by:Juliana Viccari <juliana.z.viccari@gmail.com>

--- main.go
+++ main.go
@@ -2,15 +2,37 @@ package main
 
 import (
 	"fmt"
+	re "regexp"
 )
 
 func main() {
 	userInput := Input()
-	fmt.Println(userInput)
+	token := Tokanizer(userInput)
+	fmt.Println(token)
 }
 
 func Input() string {
 	var userInput string
 	fmt.Scan(&userInput)
 	return userInput
+}
+func Tokanizer(token string) []string {
+
+	regex := re.MustCompile(`[\d]{1,}|[^\d]`)
+	numbers := re.MustCompile(`\d`)
+	arrayCharacters := regex.FindAllString(token, -1)
+	var arrayNumbersAndSignals []string
+	for i := 0; i < len(arrayCharacters); i++ {
+		str := arrayCharacters[i]
+		if str == "+" || str == "-" && numbers.Match([]byte(arrayCharacters[i+1])) {
+			strConcat := str + arrayCharacters[i+1]
+			arrayNumbersAndSignals = append(arrayNumbersAndSignals, strConcat)
+			arrayCharacters = arrayCharacters[i+2:]
+			i = -1
+		} else {
+			arrayNumbersAndSignals = append(arrayNumbersAndSignals, str)
+		}
+	}
+
+	return arrayNumbersAndSignals
 }
